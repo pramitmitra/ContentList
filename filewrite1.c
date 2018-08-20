6da582087ac5791cc3a7561a8231236ea6be65d9
pramitmitra <pramit.mitra@hotmail.com>
2018-08-19 19:12:51 -0700

    git@github.com:pramitmitra/c.git

--- filewrite1.c
+++ filewrite1.c
@@ -16,6 +16,7 @@ int main()
    scanf("%d",&num);
 
    fprintf(fptr,"%d",num);
+  //fprintf(fptr, "\s"," ");
    fclose(fptr);
 
    return 0;
--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@ int main()
 {
 printf("Hello, World!\n");
 //Array
-int nums[5] = [1,2,3,4,5]
+//int nums[5] = [1,2,3,4,5]
 int i;
 for( i = 10; i < 20; i = i + 1 )
 {
